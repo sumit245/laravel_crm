81e9d6992e6289fa56b7d7b42525ef61c7ac3439
root <root@ip-172-31-12-226.ap-south-1.compute.internal>
2024-12-20 08:39:25 +0000

    message

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -109,7 +109,7 @@ class ProjectsController extends Controller
     'project_in_state'  => 'string',
     'start_date'        => 'required|date',
     'end_date'          => 'required|date',
-    'work_order_number' => 'required|string|unique:projects',
+    'work_order_number' => 'required',
     'rate'              => 'nullable|string',
     'project_capacity'  => 'nullable|string',
     'total'             => 'string',
