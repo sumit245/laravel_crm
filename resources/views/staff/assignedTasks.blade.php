bdef57bf1d1d202c3d34faf0018c275fa739a3e5
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-03-18 15:21:41 +0530

    message

---
+++ resources/views/staff/assignedTasks.blade.php
@@ -0,0 +1,78 @@
+<div class="home-tab">
+  <div class="d-flex align-items-center flex-row">
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item" role="presentation">
+        <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#overview" role="tab"
+          aria-controls="overview" aria-selected="true">Assigned Panchayats</a>
+      </li>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#audiences" role="tab"
+          aria-controls="audiences" aria-selected="false">Surveyed Poles</a>
+      </li>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#demographics" role="tab"
+          aria-controls="demographics" aria-selected="false">Installed Poles</a>
+      </li>
+    </ul>
+  </div>
+  <div class="tab-content tab-content-basic">
+    <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="home-tab">
+      {{-- TODO: Modify the table for datatable functions as search, sort, filter, pagination, export assigned panchayats --}}
+      {{-- <table id="siteTable" class="table-striped table-bordered table-sm table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Panchayat</th>
+            <th>Block</th>
+            <th>District</th>
+            <th>Engineer</th>
+            <th>Vendor</th>
+            <th>Wards</th>
+            <th>Number of poles</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table> --}}
+    </div>
+    <div class="tab-pane fade" id="audiences" role="tabpanel" aria-labelledby="profile-tab">
+      {{-- TODO: Modify the table for datatable functions as search, sort, filter, pagination, export assigned panchayats --}}
+      {{-- <table id="siteTable" class="table-striped table-bordered table-sm table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Complete pole number</th>
+            <th>Location</th>
+            <th>Beneficiary</th>
+            <th>Remarks</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table> --}}
+    </div>
+    <div class="tab-pane fade" id="demographics" role="tabpanel" aria-labelledby="contact-tab">
+      {{-- TODO: Modify the table for datatable functions as search, sort, filter, pagination, export assigned panchayats --}}
+      {{-- <table id="siteTable" class="table-striped table-bordered table-sm table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Panchayat</th>
+            <th>Block</th>
+            <th>District</th>
+            <th>Engineer</th>
+            <th>Vendor</th>
+            <th>Wards</th>
+            <th>Number of poles</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table> --}}
+    </div>
+  </div>
+</div>
