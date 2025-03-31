1aaf1138a910cf2e6d467c4f7b6775236e7cfdb9
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-03-31 16:50:12 +0530

    Production commit

--- resources/views/jicr/show.blade.php
+++ resources/views/jicr/show.blade.php
@@ -1,132 +1,126 @@
-<html lang="en">
+<div class="pdf-container">
+  <h1 class="font-weight-bold mb-4 text-center">JOINT INSPECTION-CUM-COMMISSIONING REPORT (JICR)</h1>
+  <p class="mb-4">
+    It is hereby certified that following Solar Street Light System installed & commissioned with Remote Monitoring
+    System (RMS) on the identified existing electric pole under ‘Mukhyamantri Gramin Solar Street Light Yojana’ in
+    Bihar State” with following details has been installed and commissioned at site:
+  </p>
+  <ul class="list-unstyled mb-4">
+    <li>Name of District/ Block/ Panchayat....................................</li>
+    <li>Name of Executing Agency....................................</li>
+    <li>Work order no. and date............................</li>
+  </ul>
+  <table class="table-bordered mb-4 table">
+    <thead>
+      <tr>
+        <th>Sl.</th>
+        <th>Items</th>
+        <th>Details</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1.</td>
+        <td>Name of system: (Solar Street Light)</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>2.</td>
+        <td>Agreement number & Date:</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>3.</td>
+        <td>Capacity of System:</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>4.</td>
+        <td>
+          Name of Executing Agency with full address:<br>
+          Contact person name and mobile no –Mandatory
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>5.</td>
+        <td>
+          Name of Beneficiary with full address:<br>
+          District:<br>
+          Pin code:
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>6.</td>
+        <td>
+          Exact location on installation Latitude and longitude<br>
+          exactly One photo of system showing longitude and<br>
+          latitude in photo
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>7.</td>
+        <td>Date of installation:</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>8.</td>
+        <td>Warrantee expire on:</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>9.</td>
+        <td>
+          Name & Address of the local service Centre:<br>
+          Contact person name with mobile no -<br>
+          Mandatory Email address:
+        </td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+  <h2 class="font-weight-bold mb-2 text-center">DETAILS OF INSTALLATION OF SOLAR MODULES</h2>
+  <table class="table-bordered table">
+    <thead>
+      <tr>
+        <th>10.</th>
+        <th>a) SPV Modules Type (Poly/Mono crystalline)</th>
+        <th>a)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td>b) Sr. No of module:</td>
+        <td>b)</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>c) Make of module:</td>
+        <td>c)</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>d) Model No:</td>
+        <td>d)</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>e) Module wattage</td>
+        <td>e)</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Joint Inspection-Cum-Commissioning Report (JICR)</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-
-  <body class="p-4">
-    <div class="container">
-      <h1 class="font-weight-bold mb-4 text-center">JOINT INSPECTION-CUM-COMMISSIONING REPORT (JICR)</h1>
-      <p class="mb-4">
-        It is hereby certified that following Solar Street Light System installed & commissioned with Remote Monitoring
-        System (RMS) on the identified existing electric pole under ‘Mukhyamantri Gramin Solar Street Light Yojana’ in
-        Bihar State” with following details has been installed and commissioned at site:
-      </p>
-      <ul class="list-unstyled mb-4">
-        <li>Name of District/ Block/ Panchayat....................................</li>
-        <li>Name of Executing Agency....................................</li>
-        <li>Work order no. and date............................</li>
-      </ul>
-      <table class="table-bordered mb-4 table">
-        <thead>
-          <tr>
-            <th>Sl.</th>
-            <th>Items</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1.</td>
-            <td>Name of system: (Solar Street Light)</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>2.</td>
-            <td>Agreement number & Date:</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>3.</td>
-            <td>Capacity of System:</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>4.</td>
-            <td>
-              Name of Executing Agency with full address:<br>
-              Contact person name and mobile no –Mandatory
-            </td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>5.</td>
-            <td>
-              Name of Beneficiary with full address:<br>
-              District:<br>
-              Pin code:
-            </td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>6.</td>
-            <td>
-              Exact location on installation Latitude and longitude<br>
-              exactly One photo of system showing longitude and<br>
-              latitude in photo
-            </td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>7.</td>
-            <td>Date of installation:</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>8.</td>
-            <td>Warrantee expire on:</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>9.</td>
-            <td>
-              Name & Address of the local service Centre:<br>
-              Contact person name with mobile no -<br>
-              Mandatory Email address:
-            </td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-      <h2 class="font-weight-bold mb-2 text-center">DETAILS OF INSTALLATION OF SOLAR MODULES</h2>
-      <table class="table-bordered table">
-        <thead>
-          <tr>
-            <th>10.</th>
-            <th>a) SPV Modules Type (Poly/Mono crystalline)</th>
-            <th>a)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td>b) Sr. No of module:</td>
-            <td>b)</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>c) Make of module:</td>
-            <td>c)</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>d) Model No:</td>
-            <td>d)</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>e) Module wattage</td>
-            <td>e)</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  </body>
-
-</html>
+<style>
+  .pdf-container {
+    width: 100%;
+    margin: 0 auto;
+    padding: 20px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+  }
+</style>
