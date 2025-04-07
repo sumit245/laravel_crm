b1e4f755ba67bcfda08fb9c16edeb518c3c20410
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-04-07 14:37:56 +0530

    message

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -10,44 +10,6 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    //
-    // public function login(Request $request)
-    // {
-    //     $request->validate([
-    //         'email'    => 'required|string',
-    //         'password' => 'required|string',
-    //     ]);
-
-    //     $user = User::where('email', $request->email)->first();
-
-    //     if (!$user || !Hash::check($request->password, $user->password)) {
-    //         return response()->json(['error' => 'credentialError', 'status' => 401], 401);
-    //     }
-
-    //     $token = $user->createToken('authToken')->accessToken;
-
-    //     return response()->json([
-    //         'message' => 'Login successful',
-    //         'token'   => $token,
-    //         'user'    => [
-    //             'id'           => $user->id,
-    //             'firstName'    => $user->firstName,
-    //             'lastName'     => $user->lastName,
-    //             'name'         => $user->name,
-    //             'role'         => $user->role,
-    //             'email'        => $user->email,
-    //             'image'        => $user->image,
-    //             'status'       => $user->status,
-    //             'disableLogin' => $user->disableLogin,
-    //             'address'      => $user->address,
-    //             'contactNo'    => $user->contactNo,
-    //             'lastOnline'   => $user->lastOnline,
-    //             'created_at'   => $user->created_at,
-    //             'updated_at'   => $user->updated_at,
-    //         ],
-    //         'status'  => 200,
-    //     ]);
-    // }
 
     public function login(Request $request)
     {
