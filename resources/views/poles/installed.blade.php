e31eb8e45bdbde5d75307d141751935ada7e52ea
yvsingh-code <yashvir@dashanddots.tech>
2025-04-08 10:35:36 +0530

    fiftyfour-commit

--- resources/views/poles/installed.blade.php
+++ resources/views/poles/installed.blade.php
@@ -5,24 +5,40 @@
     <h3 class="fw-bold">Installed Poles</h3>
     <p>Total Installed Poles: <strong>{{ $totalInstalled }}</strong></p>
 
-    <x-data-table id="installedPoles" class="table-striped table">
+    <x-data-table id="installedPole" class="table-striped table">
       <x-slot:thead>
         <tr>
-          <th>Pole ID</th>
-          <th>Complete Pole Number</th>
-          <th>Sim Number</th>
-          <th>Latitude</th>
-          <th>Longitude</th>
+        <th data-select="true">
+          <input type="checkbox" id="selectAll" />
+        </th>
+        <th>#</th>
+        <th>Complete Pole Numbers</th>
+        <th>Location</th>
+        <th>Sim Numbers</th>
+        <th>Luminary QR</th>
+        <th>Battery QR</th>
+        <th>Panel QR</th>
+        <th>RMS status</th>
+        <th>Actions</th>
         </tr>
       </x-slot:thead>
       <x-slot:tbody>
         @foreach ($poles as $pole)
           <tr>
-            <td>{{ $pole->id }}</td>
-            <td>{{ $pole->complete_pole_number }}</td>
-            <td>{{ $pole->sim_number }}</td>
-            <td>{{ $pole->lat }}</td>
-            <td>{{ $pole->lng }}</td>
+          <td>{{ $pole->id }}</td>
+          <td>{{ $pole->complete_pole_number ?? "N/A" }}</td>
+          <td>{{ $pole->lat && $survey->lng ? $survey->lat .', '. $survey->lng : "N/A" }}</td>
+          <td>{{ $pole->sim_number ?? "N/A" }}</td>
+          <td>{{ $pole->luminary_qr ?? "N/A" }}</td>
+          <td>{{ $pole->battery_qr ?? "N/A" }}</td>
+          <td>{{ $pole->panel_qr ?? "N/A" }}</td>
+          <td>{{ $pole->be ?? "N/A" }}</td>
+          <td>
+            <!-- View Button -->
+            <a href="{{-- route("inventory.show", $member->id) --}}" class="btn btn-icon btn-info" data-toggle="tooltip" title="View Details">
+              <i class="mdi mdi-eye"></i>
+            </a>
+          </td>
           </tr>
         @endforeach
       </x-slot:tbody>
