268d62aa8be17f105fe7128f1a0c41bc74aedcaa
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-04-15 15:36:07 +0530

    Added table roles"

---
+++ resources/views/tasks/show_streetlight.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.main")
+
+@section("content")
+  <pre>{{ $streetlightTask }}</pre>
+  <pre>{{ $manager }}</pre>
+  <pre>{{ $engineer }}</pre>
+  <pre>{{ $vendor }}</pre>
+  <pre>{{ $streetlight }}</pre>
+  <pre>{{ $surveyedPoles }}</pre>
+  <pre>{{ $installedPoles }}</pre>
+@endsection
