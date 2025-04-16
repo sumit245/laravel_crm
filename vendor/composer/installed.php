0e5fe6343bb9515be5a547d4bf5490b887c2721f
root <root@ip-172-31-12-226.ap-south-1.compute.internal>
2025-04-16 07:52:10 +0000

    message

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '33c9d64c87e5bb7d996b3d814f6f4ec0e1004e64',
+        'reference' => '3962bfa5f3b4b3dfc8c337e107c592f4c94196b1',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -481,7 +481,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '33c9d64c87e5bb7d996b3d814f6f4ec0e1004e64',
+            'reference' => '3962bfa5f3b4b3dfc8c337e107c592f4c94196b1',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -1054,8 +1054,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
