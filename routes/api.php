a8eaff098f46337c17f0f7d4a2aa1bfcc790ca72
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-04-17 09:20:45 +0530

    message

--- routes/api.php
+++ routes/api.php
@@ -40,10 +40,10 @@ Route::apiResource('task', TaskController::class);
 Route::post('/tasks/{id}/approve', [TaskController::class, 'approveTask']);
 Route::get('/get_installable_pole/{ward}', [TaskController::class, 'getInstallablePoles']);
 Route::post('streetlight/tasks/update', [TaskController::class, 'submitStreetlightTasks']);
-Route::post('send-to-rms', [TaskController::class, 'sendDataToRMS']);
 Route::get('streetlight/tasks/engineers', [StreetlightController::class, 'getEngineerTasks']);
 Route::get('streetlight/tasks/vendors', [StreetlightController::class, 'getVendorTasks']);
 Route::apiResource('streetlight', StreetlightController::class);
+Route::post('send-to-rms', [TaskController::class, 'sendDataToRMS']);
 
 Route::apiResource('inventories', InventoryController::class);
 Route::post('inventory/dispatch/vendor', [InventoryController::class, 'dispatchInventory']);
