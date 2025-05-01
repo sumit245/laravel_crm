375c5696fd9561c00571ea7820732756309c6385
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-05-01 14:15:34 +0530

    message

--- app/Http/Controllers/API/ConveyanceController.php
+++ app/Http/Controllers/API/ConveyanceController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Conveyance;
@@ -84,22 +84,22 @@ class ConveyanceController extends Controller
         ]);
 
         // Handle start_journey_pnr files
-    // if ($request->hasFile('start_journey_pnr')) {
-    //     $startFiles = [];
-    //     foreach ($request->file('start_journey_pnr') as $file) {
-    //         $startFiles[] = $file->store('pnrs/start', 'public');
-    //     }
-    //     $data['start_journey_pnr'] = $startFiles;
-    // }
+        // if ($request->hasFile('start_journey_pnr')) {
+        //     $startFiles = [];
+        //     foreach ($request->file('start_journey_pnr') as $file) {
+        //         $startFiles[] = $file->store('pnrs/start', 'public');
+        //     }
+        //     $data['start_journey_pnr'] = $startFiles;
+        // }
 
-    // Handle end_journey_pnr files
-    // if ($request->hasFile('end_journey_pnr')) {
-    //     $endFiles = [];
-    //     foreach ($request->file('end_journey_pnr') as $file) {
-    //         $endFiles[] = $file->store('pnrs/end', 'public');
-    //     }
-    //     $data['end_journey_pnr'] = $endFiles;
-    // }
+        // Handle end_journey_pnr files
+        // if ($request->hasFile('end_journey_pnr')) {
+        //     $endFiles = [];
+        //     foreach ($request->file('end_journey_pnr') as $file) {
+        //         $endFiles[] = $file->store('pnrs/end', 'public');
+        //     }
+        //     $data['end_journey_pnr'] = $endFiles;
+        // }
 
         $conveyance = Conveyance::create($data);
 
