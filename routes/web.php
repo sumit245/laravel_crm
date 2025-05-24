8da97e0d5a4d1fe1794cd19f5ececaa2764d8567
yvsingh-code <yashvir@dashanddots.tech>
2025-05-24 16:18:18 +0530

    h40-commit

--- routes/web.php
+++ routes/web.php
@@ -130,7 +130,7 @@ Route::middleware(['auth'])->group(function () {
 
     // Task router
     Route::resource('tasks', TasksController::class)->except(['show']);
-    Route::get('/tasks/rooftop/{id}', [TasksController::class, 'rooftop'])->name('tasks.rooftop');
+    Route::get('/tasks/rooftop/{id}', [TasksController::class, 'editrooftop'])->name('tasks.editrooftop');
     Route::post('/tasks/rooftop/update/{id}', [TasksController::class, 'updateRooftop'])->name('tasks.updaterooftop');
     // Greedy path
     Route::get('/tasks/{id}/{any?}', [TasksController::class, 'show'])->where('any', '.*')->name('tasks.show');
