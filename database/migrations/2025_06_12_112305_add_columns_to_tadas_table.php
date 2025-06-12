07dad3cdc5d7bb9cca5cd6f36f1e68f11b46307d
yvsingh-code <yashvir@dashanddots.tech>
2025-06-12 20:55:07 +0530

    h61-commit

---
+++ database/migrations/2025_06_12_112305_add_columns_to_tadas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tadas', function (Blueprint $table) {
+            // Drop old columns
+            $table->dropColumn(['objective_tour', 'outcome_achieve']);
+
+            // Add new columns
+            $table->string('visiting_to')->nullable()->after('status');
+            $table->string('purpose_of_visit')->nullable()->after('visiting_to');
+            $table->string('outcome_achieved')->nullable()->after('purpose_of_visit');
+            $table->date('date_of_departure')->nullable()->after('outcome_achieved');
+            $table->date('date_of_return')->nullable()->after('date_of_departure');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tadas', function (Blueprint $table) {
+            //
+        });
+    }
+};
