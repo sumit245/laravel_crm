8455bca022f2b283ae8d9090f2161c2b2fcd3b62
sumit245 <sumitranjan245@gmail.com>
2025-09-23 15:44:26 +0530

    Fixing Issues

--- routes/api.php
+++ routes/api.php
@@ -72,3 +72,5 @@ Route::get('user-category', [ConveyanceController::class, 'getUserCategoryVehicl
 Route::get('user-category/{id}', [ConveyanceController::class, 'getUserCategory']);
 Route::get('check-price', [ConveyanceController::class, 'checkPice']);
 Route::get('allow-expense', [ConveyanceController::class, 'allowExpense']);
+
+
