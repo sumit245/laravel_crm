2f84048200552f1b13d795755bb30bd2b4a3ba68
sumit245 <sumitranjan245@gmail.com>
2026-01-14 17:01:18 +0000

    master

--- app/Helpers/RemoteApiHelper.php
+++ app/Helpers/RemoteApiHelper.php
@@ -12,20 +12,25 @@ class RemoteApiHelper
     {
         $url = env('RMS_API_URL', 'https://ssl.slldm.com/insertMasterData.php');
         $districtCode = DistrictCode::where('district_name', strtoupper(trim($streetlight->district)))->value('district_code');
-       Log::info( basename($pole->complete_pole_number));
+	$wardType = ($pole->ward_name ==="GP") ? 'GP' : 'W';
+        $poleName = $wardType === 'GP' 
+            ? (($digits = preg_replace('/\D/', '', $pole->complete_pole_number)) && strlen($digits) >= 2 
+                ? substr($digits, -2) 
+                : substr($pole->complete_pole_number, strrpos($pole->complete_pole_number, '/') + 1))
+            : substr($pole->complete_pole_number, strrpos($pole->complete_pole_number, '/') + 1);
         $payload = [
             'devId' => $pole->luminary_qr,
             'MfId' => "4",
-            'poleName' => $pole->complete_pole_number,
-	'projectName'=>"SUGS",
+            'poleName' => $poleName,
+	    'project'=>"SUGS",
             'district' => $streetlight->district,
             'districtCode' => (string) $districtCode,
             'block' => $streetlight->block,
             'blockCode' => $streetlight->block_code,
             'panchayat' => $streetlight->panchayat,
             'panchayatCode' => $streetlight->panchayat_code,
-            'ward_type' => $streetlight->ward_type ?? 'W',
-            'ward_number' => "02",
+            'ward_type' => $wardType,
+            'ward_number' => $wardType==='GP'?null:$pole->ward_name,
             'BattSno' => $pole->battery_qr,
             'PvSno' => $pole->panel_qr,
             'simNo' => $pole->sim_number,
